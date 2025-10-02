21ba30980db7d5d80f5c6b905fe77959f99d4496
Babaloo Patel <lazy.lion.bpatel@gmail.com>
2025-10-02 12:27:26 +0530

    test for string having \n

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -19,4 +19,8 @@ RSpec.describe StringCalculator do
   it "returns sum for multiple numbers" do
     expect(sc.add("1,2,3,4")).to eq(10)
   end
+
+  it "handles newlines between numbers" do
+    expect(sc.add("1\n2,3")).to eq(6)
+  end
 end
