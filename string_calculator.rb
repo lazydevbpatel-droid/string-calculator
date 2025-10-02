614c75d0ab2c00b5ee1731a6a0123a28fb070ed0
Babaloo Patel <lazy.lion.bpatel@gmail.com>
2025-10-02 22:12:13 +0530

    refactor: move negative number handling logic above delimiter assignment

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,8 +3,14 @@ class StringCalculator
         # when integer_string is empty
         return 0 if integer_string.empty?
 
-        # when integer_string has more than 1 integer empty
         delimiter = /,|\n/
+
+        # when integer_string has negative numbers
+        integer_array = integer_string.split(delimiter).map(&:to_i)
+        negatives = integer_array.select { |n| n < 0 }
+        raise "negatives not allowed: #{negatives.join(', ')}" if negatives.any?
+
+        # when integer_string has more than 1 integer empty
         if integer_string.start_with?("//")
             parts = integer_string.split("\n", 2)
             delimiter = Regexp.new(Regexp.escape(parts[0][2..]))
